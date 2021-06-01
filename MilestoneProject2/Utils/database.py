f9f18bf4ad9cb00c987414cb548c01b3d3a36620
julienPalleau <jpalleau@gmail.com>
2021-06-01 08:19:17 +0200

    organising and finshing the import

---
+++ MilestoneProject2/Utils/database.py
@@ -0,0 +1,21 @@
+"""
+Concerned with storing and retrieving books from a list.
+"""
+
+books = []
+
+
+def add_book(name, author):
+    books.append({'name': name, 'author': author, 'read': False})
+
+
+def list_books():
+    pass
+
+
+def mark_book():
+    pass
+
+
+def delete_book():
+    pass
