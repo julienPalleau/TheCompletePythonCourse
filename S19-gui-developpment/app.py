b918ed8047f2ef1ee999d0d454cb34a70be2a8e4
julienPalleau <jpalleau@gmail.com>
2021-08-21 11:02:49 +0200

    Adding a permanent scrollbar to our text area

--- S19-gui-developpment/app.py
+++ S19-gui-developpment/app.py
@@ -106,14 +106,21 @@ text_contents = dict()
 
 
 def create_file(content="", title="Untitled"):
-    text_area = tk.Text(notebook)
-    text_area.insert("end", content)
-    text_area.pack(fill="both", expand=True)
+    container = ttk.Frame(notebook)
+    container.pack()
 
-    notebook.add(text_area, text=title)
-    notebook.select(text_area)
+    text_area = tk.Text(container)
+    text_area.insert("end", content)
+    text_area.pack(side="left", fill="both", expand=True)
+
+    notebook.add(container, text=title)
+    notebook.select(container)
 
     text_contents[str(text_area)] = hash(content)
+
+    text_scroll = ttk.Scrollbar(container, orient="vertical", command=text_area.yview)
+    text_scroll.pack(side="right", fill="y")
+    text_area["yscrollcommand"] = text_scroll.set
 
 
 def check_for_changes():
@@ -129,7 +136,8 @@ def check_for_changes():
 
 
 def get_text_widget():
-    text_widget = root.nametowidget(notebook.select())
+    tab_widget = root.nametowidget(notebook.select())
+    text_widget = tab_widget.winfo_children()[0]
     return text_widget
 
 
@@ -162,7 +170,8 @@ def confirm_quit():
     unsaved = False
 
     for tab in notebook.tabs():
-        text_widget = root.nametowidget(tab)
+        tab_widget = root.nametowidget(tab)
+        text_widget = tab_widget.winfo_children()[0]
         content = text_widget.get("1.0", "end-1c")
 
         if hash(content) != text_contents[str(text_widget)]:
@@ -218,6 +227,13 @@ def open_file():
     create_file(content, filename)
 
 
+def show_about_info():
+    messagebox.showinfo(
+        title="About",
+        message="The Text Editor is a simple tabbed text ditor designed to help you learn Tkinter."
+    )
+
+
 root = tk.Tk()
 root.title("Text Editor")
 root.option_add("*tearOff", False)
@@ -229,13 +245,17 @@ menubar = tk.Menu()
 root.config(menu=menubar)
 
 file_menu = tk.Menu(menubar)
+help_menu = tk.Menu(menubar)
 menubar.add_cascade(menu=file_menu, label="File")
+menubar.add_cascade(menu=help_menu, label="Help")
 
 file_menu.add_command(label="New", command=create_file, accelerator="Ctrl+N")
 file_menu.add_command(label="Open...", command=open_file, accelerator="Ctrl+O")
 file_menu.add_command(label="Save", command=save_file, accelerator="Ctrl+S")
 file_menu.add_command(label="Close Tab", command=close_current_tab, accelerator="Ctrl+Q")
 file_menu.add_command(label="Exit", command=confirm_quit)
+
+help_menu.add_command(label="About", command=show_about_info)
 
 notebook = ttk.Notebook(main)
 notebook.pack(fill="both", expand=True)
