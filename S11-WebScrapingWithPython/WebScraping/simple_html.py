e464a99a7775bc7c255588469bfcfb88472fce11
julienPalleau <jpalleau@gmail.com>
2021-08-25 20:50:06 +0200

    beatifulSoup More complex html parsing

--- S11-WebScrapingWithPython/WebScraping/simple_html.py
+++ S11-WebScrapingWithPython/WebScraping/simple_html.py
@@ -17,4 +17,30 @@ SIMPLE_HTML = '''<html>
 
 simple_soup = BeautifulSoup(SIMPLE_HTML, 'html.parser')
 
-print(simple_soup.find('h1'))
+
+def find_title():
+    h1_tag = simple_soup.find('h1')
+    print(h1_tag.string)
+
+
+def find_list_items():
+    list_items = simple_soup.find_all('li')
+    list_contents = [e.string for e in list_items]
+    print(list_contents)
+
+
+def find_subtitle():
+    paragraph = simple_soup.find('p', {'class': 'subtitle'})
+    print(paragraph.string)
+
+
+def find_other_paragraph():
+    paragraphs = simple_soup.find_all('p')
+    other_paragraph = [p for p in paragraphs if 'subtitle' not in p.attrs.get('class', [])]
+    print(other_paragraph[0].string)
+
+
+find_title()
+find_list_items()
+find_subtitle()
+find_other_paragraph()
