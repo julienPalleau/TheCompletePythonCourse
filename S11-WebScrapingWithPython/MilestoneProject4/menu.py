67625bfd5f8ed22b7c104d6bdb88eb10ecaf44ca
julienPalleau <jpalleau@gmail.com>
2021-08-28 10:59:57 +0200

    Books project 12 (MilestoneProject4)

--- S11-WebScrapingWithPython/MilestoneProject4/menu.py
+++ S11-WebScrapingWithPython/MilestoneProject4/menu.py
@@ -1,28 +1,69 @@
+import logging
+
 from app import books
+
+logger = logging.getLogger('scrapping.menu')
+
+USER_CHOICE = '''Enter on of the following
+- 'b' to look at ordered books by number of stars
+- 'c' to look at the cheapest books
+- 'n' to just get the next available book on the catalogue
+- 't' list of books ordered by price and stars
+- 'q' to exit
+
+Enter your choice: '''
 
 
 def print_best_books():
+    logger.info('Finding best books by rating...')
     best_books = sorted(books, key=lambda x: x.rating * -1)[:10]
     for book in best_books:
         print(book)
+    print("\n")
 
 
 def print_cheapest_books():
+    logger.info('Finding best books by price...')
     cheapest_books = sorted(books, key=lambda x: x.price)[:10]
     for book in cheapest_books:
         print(book)
+    print("\n")
 
 
 # Sorting by 2 criteria
 def print_two_criteria():
+    logger.info('Finding best books by two criteria...')
     two_criteria = sorted(books, key=lambda x: (x.rating * -1, x.price))[:10]
     for book in two_criteria:
         print(book)
+    print("\n")
 
 
-print('----- BEST -----')
-print_best_books()
-print('----- CHEAPEST -----')
-print_cheapest_books()
-print('----- TWO CRITERIA -----')
-print_two_criteria()
+books_generator = (x for x in books)
+
+
+def get_next_book():
+    logger.info('Getting next book from generator of all books...')
+    print(next(books_generator))
+
+
+user_choices = {
+    'b': print_best_books,
+    'c': print_cheapest_books,
+    'n': get_next_book,
+    't': print_two_criteria,
+}
+
+
+def menu():
+    user_input = input(USER_CHOICE)
+    while user_input != 'q':
+        if user_input in ('b', 'c', 'n', 't', 'x'):
+            user_choices[user_input]()
+        else:
+            print('Please choose a valid command.')
+        user_input = input(USER_CHOICE)
+    logger.debug('Terminating program...')
+
+
+menu()
--- S11-WebScrapingWithPython/MilestoneProject4/parsers/book_parser.py
+++ S11-WebScrapingWithPython/MilestoneProject4/parsers/book_parser.py
@@ -1,5 +1,8 @@
 import re
+import logging
 from MilestoneProject4.locators.book_locators import BookLocators
+
+logger = logging.getLogger('scraping.book_parser')
 
 
 class BookParser:
@@ -16,6 +19,7 @@ class BookParser:
     }
 
     def __init__(self, parent):
+        logger.debug(f'New book parser created from `{parent}`.')
         self.parent = parent
 
     def __repr__(self):
@@ -23,32 +27,41 @@ class BookParser:
 
     @property
     def name(self):
+        logger.debug('Finding book name...')
         locator = BookLocators.NAME_LOCATOR
         item_link = self.parent.select_one(locator)
         item_name = item_link.attrs['title']
+        logger.debug(f'Found book name, `{item_name}`.')
         return item_name
 
     @property
     def link(self):
+        logger.debug('Finding book link...')
         locator = BookLocators.LINK_LOCATOR
         item_link = self.parent.select_one(locator).attrs['href']
+        logger.debug(f'Found book link, `{item_link}`.')
         return item_link
 
     @property
     def price(self):
+        logger.debug('Finding book price...')
         locator = BookLocators.PRICE_LOCATOR
         item_price = self.parent.select_one(locator).string  # £51.77
         pattern = '£([0-9]+\.[0-9]+)'
         matcher = re.search(pattern, item_price)
-        return float(matcher.group(1))  # £51.77
+        float_price = float(matcher.group(1))
+        logger.debug(f'Found book price, `{float_price}`.')
+        return float_price  # £51.77
 
     @property
     def rating(self):
+        logger.debug('Finding book rating...')
         locator = BookLocators.RATING_LOCATOR
         star_rating_tag = self.parent.select_one(locator)
         classes = star_rating_tag.attrs['class']  # ['star-rating', 'Three']
         rating_classes = [r for r in classes if r != 'star-rating']  # one way to do it
         # rating_classes = filter(lambda x: x != 'star-rating', classes) # second way to do it
         rating_number = BookParser.RATINGS.get(rating_classes[0])  # None if not found
+        logger.debug(f'Found book rating, `{rating_number}`.')
         return rating_number
 
