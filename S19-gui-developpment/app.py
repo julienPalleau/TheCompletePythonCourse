b5b401a6ff600e93220cabbd4291f5dc4e9ed4f1
julienPalleau <jpalleau@gmail.com>
2021-09-21 16:40:00 +0200

    fix bug when closing a tab which is the las one opened

--- S19-gui-developpment/app.py
+++ S19-gui-developpment/app.py
@@ -3,8 +3,8 @@
 # --------------------------------------------------------------------------------------------------------------
 # import tkinter as tk
 # from tkinter import ttk
-
-
+#
+#
 # def greet():
 #     print("Hello, world")
 #
@@ -56,19 +56,20 @@
 
 # tk.Label(root, text="Lable 1", bg="green").pack(side="left")
 # tk.Label(root, text="Lable 2", bg="red").pack(side="top")
-# -------------
+# # -------------
 # tk.Label(root, text="Lable 1", bg="green").pack(side="left", fill="y")
 # tk.Label(root, text="Lable 2", bg="red").pack(side="top", fill="x")
-# -------------
+# # -------------
 # tk.Label(root, text="Lable 1", bg="green").pack(side="left", fill="both")
 # tk.Label(root, text="Lable 2", bg="red").pack(side="top", fill="both")
-# -------------
+# # -------------
 # tk.Label(root, text="Lable 1", bg="green").pack(side="left", fill="both", expand=True)
 # tk.Label(root, text="Lable 2", bg="red").pack(side="top")
-# -------------
-# tk.Label(root, text="Label left", bg="green").pack(
-#     side="left", expand=True, fill="both"
-# )
+# # -------------
+# tk.Label(root, text="Lable 1", bg="green").pack(side="top", fill="both", expand=True)
+# tk.Label(root, text="Lable 2", bg="red").pack(side="top", fill="both", expand=True)
+# # -------------
+# tk.Label(root, text="Label left", bg="green").pack(side="left", expand=True, fill="both")
 #
 # tk.Label(root, text="Lable top", bg="red").pack(side="top", fill="both", expand=True)
 # tk.Label(root, text="Lable top", bg="red").pack(side="top", fill="both", expand=True)
@@ -97,177 +98,162 @@
 # # --------------------------------------------------------------------------------------------------------------
 # # Starting our text editor project
 # # --------------------------------------------------------------------------------------------------------------
-# import os
-# import tkinter as tk
-# from tkinter import ttk, filedialog, messagebox
-#
-#
-# text_contents = dict()
-#
-#
-# def create_file(content="", title="Untitled"):
-#     container = ttk.Frame(notebook)
-#     container.pack()
-#
-#     text_area = tk.Text(container)
-#     text_area.insert("end", content)
-#     text_area.pack(side="left", fill="both", expand=True)
-#
-#     notebook.add(container, text=title)
-#     notebook.select(container)
-#
-#     text_contents[str(text_area)] = hash(content)
-#
-#     text_scroll = ttk.Scrollbar(container, orient="vertical", command=text_area.yview)
-#     text_scroll.pack(side="right", fill="y")
-#     text_area["yscrollcommand"] = text_scroll.set
-#
-#
-# def check_for_changes():
-#     current = get_text_widget()
-#     content = current.get("1.0", "end-1c")
-#     name = notebook.tab("current")["text"]
-#
-#     if hash(content) != text_contents[str(current)]:
-#         if name[-1] != "*":
-#             notebook.tab("current", text=name + "*")
-#     elif name[-1] == "*":
-#         notebook.tab("current", text=name[:-1])
-#
-#
-# def get_text_widget():
-#     tab_widget = root.nametowidget(notebook.select())
-#     text_widget = tab_widget.winfo_children()[0]
-#     return text_widget
-#
-#
-# def close_current_tab():
-#     current = get_text_widget()
-#     if current_tab_unsaved() and not confirm_close():
-#         return
-#
-#     if len(notebook.tabs()) == 1:
-#         create_file()
-#
-#     notebook.forget(current)
-#
-#
-# def current_tab_unsaved():
-#     text_widget = get_text_widget()
-#     content = text_widget.get("1.0", "end-1c")
-#     return hash(content) != text_contents[str(text_widget)]
-#
-#
-# def confirm_close():
-#     return messagebox.askyesno(
-#         message="You have unsaved changes. Are you sure you want to close ?",
-#         icon="question",
-#         title="Unsaved Changes"
-#     )
-#
-#
-# def confirm_quit():
-#     unsaved = False
-#
-#     for tab in notebook.tabs():
-#         tab_widget = root.nametowidget(tab)
-#         text_widget = tab_widget.winfo_children()[0]
-#         content = text_widget.get("1.0", "end-1c")
-#
-#         if hash(content) != text_contents[str(text_widget)]:
-#             unsaved = True
-#             break
-#
-#     if unsaved:
-#         confirm = messagebox.askyesno(
-#             message="You have unsaved changes. Are you sure you want to quit?",
-#             icon="question",
-#             title="Confirm Quit"
-#         )
-#         if unsaved and not confirm_close():
-#             return
-#
-#     root.destroy()
-#
-#
-# def save_file():
-#     file_path = filedialog.asksaveasfilename()
-#
-#     try:
-#         filename = os.path.basename(file_path)
-#         text_widget = get_text_widget()
-#         content = text_widget.get("1.0", "end-1c")
-#
-#         with open(file_path, "w") as file:
-#             file.write(content)
-#
-#     except (AttributeError, FileNotFoundError):
-#         print("Save operation cancelled")
-#         return
-#
-#     notebook.tab("current", text=filename)
-#     text_contents[str(text_widget)] = hash(content)
-#
-#
-# def open_file():
-#     file_path = filedialog.askopenfilename(
-#         defaultextension="txt",
-#         filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")]
-#     )
-#     try:
-#         filename = os.path.basename(file_path)
-#
-#         with open(file_path, "r") as file:
-#             content = file.read()
-#
-#     except (AttributeError, FileNotFoundError):
-#         print("Open operation cancelled")
-#         return
-#
-#     create_file(content, filename)
-#
-#
-# def show_about_info():
-#     messagebox.showinfo(
-#         title="About",
-#         message="The Text Editor is a simple tabbed text ditor designed to help you learn Tkinter."
-#     )
-#
-#
-# root = tk.Tk()
-# root.title("Text Editor")
-# root.option_add("*tearOff", False)
-#
-# main = tk.Frame(root)
-# main.pack(fill="both", expand=True, padx=1, pady=(4, 0))
-#
-# menubar = tk.Menu()
-# root.config(menu=menubar)
-#
-# file_menu = tk.Menu(menubar)
-# help_menu = tk.Menu(menubar)
-# menubar.add_cascade(menu=file_menu, label="File")
-# menubar.add_cascade(menu=help_menu, label="Help")
-#
-# file_menu.add_command(label="New", command=create_file, accelerator="Ctrl+N")
-# file_menu.add_command(label="Open...", command=open_file, accelerator="Ctrl+O")
-# file_menu.add_command(label="Save", command=save_file, accelerator="Ctrl+S")
-# file_menu.add_command(label="Close Tab", command=close_current_tab, accelerator="Ctrl+Q")
-# file_menu.add_command(label="Exit", command=confirm_quit)
-#
-# help_menu.add_command(label="About", command=show_about_info)
-#
-# notebook = ttk.Notebook(main)
-# notebook.pack(fill="both", expand=True)
-# create_file()
-#
-# root.bind("<KeyPress>", lambda event: check_for_changes())
-# root.bind("<Control-n>", lambda event: create_file())
-# root.bind("<Control-o>", lambda event: open_file())
-# root.bind("<Control-q>", lambda event: close_current_tab())
-# root.bind("<Control-s>", lambda event: save_file())
-#
-# root.mainloop()
+import os
+import tkinter as tk
+from tkinter import ttk, filedialog, messagebox
+
+
+text_contents = dict()
+
+
+def check_for_changes():
+    current = get_text_widget()
+    content = current.get("1.0", "end-1c")
+    name = notebook.tab("current")["text"]
+
+    if hash(content) != text_contents[str(current)]:
+        if name[-1] != "*":
+            notebook.tab("current", text=name + "*")
+    elif name[-1] == "*":
+        notebook.tab("current", text=name[:-1])
+
+
+def close_current_tab():
+    current = get_text_widget()
+    if current_tab_unsaved() and not confirm_close():
+        return
+
+    if len(notebook.tabs()) == 1:
+        create_file()
+
+    notebook.forget(current)
+
+
+def confirm_close():
+    return messagebox.askyesno(
+        message="You have unsaved changes. Are you sure you want to close?",
+        icon="question",
+        title="Unsaved Changes"
+    )
+
+
+def confirm_quit():
+    unsaved = False
+
+    for tab in notebook.tabs():
+        text_widget = root.nametowidget(tab)
+        content = text_widget.get("1.0", "end-1c")
+
+        if hash(content) != text_contents[str(text_widget)]:
+            unsaved = True
+            break
+
+    if unsaved and not confirm_close():
+        return
+
+    root.destroy()
+
+
+def create_file(content="", title="Untitled"):
+    text_area = tk.Text(notebook)
+    text_area.insert("end", content)
+    text_area.pack(fill="both", expand=True)
+
+    notebook.add(text_area, text=title)
+    notebook.select(text_area)
+
+    text_contents[str(text_area)] = hash(content)
+
+
+def current_tab_unsaved():
+    current_tab_name = notebook.tab("current", "text")
+    return current_tab_name[-1] == "*"
+
+
+def get_text_widget():
+    text_widget = notebook.nametowidget(notebook.select())
+
+    return text_widget
+
+
+def open_file():
+    file_path = filedialog.askopenfilename(
+        defaultextension="txt",
+        filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")]
+    )
+    try:
+        filename = os.path.basename(file_path)
+
+        with open(file_path, "r") as file:
+            content = file.read()
+
+    except (AttributeError, FileNotFoundError):
+        print("Open operation cancelled")
+        return
+
+    create_file(content, filename)
+
+
+def save_file():
+    file_path = filedialog.asksaveasfilename()
+
+    try:
+        filename = os.path.basename(file_path)
+        text_widget = get_text_widget()
+        content = text_widget.get("1.0", "end-1c")  # end except the last character which is a new line
+
+        with open(file_path, "w") as file:
+            file.write(content)
+
+    except (AttributeError, FileNotFoundError):
+        print("Save operation cancelled")
+        return
+
+    notebook.tab("current", text=filename)
+    text_contents[str(text_widget)] = hash(content)
+
+
+def show_about_info():
+    messagebox.showinfo(
+        title="About",
+        message="The Text Editor is a simple tabbed text ditor designed to help you learn Tkinter."
+    )
+
+
+root = tk.Tk()
+root.title("Text Editor")
+root.option_add("*tearOff", False)  # the-complete-python-course - lecon 265
+
+main = tk.Frame(root)
+main.pack(fill="both", expand=True, padx=1, pady=(4, 0))
+
+menubar = tk.Menu()
+root.config(menu=menubar)
+
+file_menu = tk.Menu(menubar)
+help_menu = tk.Menu(menubar)
+menubar.add_cascade(menu=file_menu, label="File")
+menubar.add_cascade(menu=help_menu, label="Help")
+
+file_menu.add_command(label="New", command=create_file, accelerator="Ctrl+N")
+file_menu.add_command(label="Open...", command=open_file, accelerator="Ctrl+O")
+file_menu.add_command(label="Save", command=save_file, accelerator="Ctrl+S")
+file_menu.add_command(label="Close Tab", command=close_current_tab, accelerator="Ctrl+Q")
+file_menu.add_command(label="Exit", command=confirm_quit)
+
+help_menu.add_command(label="About", command=show_about_info)
+
+notebook = ttk.Notebook(main)
+notebook.pack(fill="both", expand=True)
+create_file()
+
+root.bind("<KeyPress>", lambda event: check_for_changes())
+root.bind("<Control-n>", lambda event: create_file())
+root.bind("<Control-o>", lambda event: open_file())
+root.bind("<Control-q>", lambda event: close_current_tab())
+root.bind("<Control-s>", lambda event: save_file())
+
+root.mainloop()
 
 ######################################## listbox ######################################
 # ------------------------------
@@ -335,74 +321,76 @@
 # ws.mainloop()
 
 # ------------------------------
-# listbox multiple choices + add an entry in the listbox
-from tkinter import *
-from tkinter import ttk
-import tkinter
-
-
-def selection():
-    if Lb1.curselection() != ():
-        for i in Lb1.curselection():
-            print(Lb1.get(i))
-    else:
-        print("Veuillez selectionner au moins une valeur")
-
-
-def start():
-    Lb1.delete(0, 5)
-    Lb1.pack()
-
-
-def recupVal():
-    # # original
-    # print(entry_field.get())
-    # Lb1.insert(Lb1.size(), entry_field.get())
-    # delete()
-
-    # # Test pour le jeux ortho
-    list1 = ["Python", "Perl", "C", "PHP", "JSP", "Ruby"]
-    list2 = []
-    for i, listbox_entry in enumerate(Lb1.get(0, END)):
-        list2.append(listbox_entry)
-
-    for element in list2:
-        if element in list1:
-            list1.remove(element)
-
-    print(f"list1 {list1}")
-    print(f"list2 {list2}")
-
-
-def delete():
-    entry_field.delete(0, 'end')
-
-
-root = Tk()
-
-Lb1 = Listbox(root, selectmode=MULTIPLE)
-Lb1.insert(1, "Python")
-Lb1.insert(2, "Perl")
-Lb1.insert(3, "C")
-Lb1.insert(4, "PHP")
-Lb1.insert(5, "JSP")
-Lb1.insert(6, "Ruby")
-
-Lb1.pack()
-
-quit_button = ttk.Button(text="Quit", command=root.destroy).pack()
-valid_button = ttk.Button(text="Validate", command=selection).pack()
-
-start_button = ttk.Button(text="Start", command=start).pack()
-
-button = Button(root, text="Valider", command=recupVal).pack()
-
-label_field = Label(root, text="add a word").pack(side=LEFT)
-
-entry_field = Entry(root, bd=5)
-entry_field.pack(side=RIGHT)
-
-root.mainloop()
+# # listbox multiple choices + add an entry in the listbox
+# from tkinter import *
+# from tkinter import ttk
+#
+#
+# # import tkinter
+#
+#
+# def selection():
+#     if Lb1.curselection() != ():
+#         for i in Lb1.curselection():
+#             print(Lb1.get(i))
+#     else:
+#         print("Veuillez selectionner au moins une valeur")
+#
+#
+# def start():
+#     Lb1.delete(0, 5)
+#     Lb1.pack()
+#
+#
+# def recupVal():
+#     # # original
+#     # print(entry_field.get())
+#     # Lb1.insert(Lb1.size(), entry_field.get())
+#     # delete()
+#
+#     # # Test pour le jeux ortho
+#     list1 = ["Python", "Perl", "C", "PHP", "JSP", "Ruby"]
+#     list2 = []
+#     for i, listbox_entry in enumerate(Lb1.get(0, END)):
+#         list2.append(listbox_entry)
+#
+#     for element in list2:
+#         if element in list1:
+#             list1.remove(element)
+#
+#     print(f"list1 {list1}")
+#     print(f"list2 {list2}")
+#
+#
+# def delete():
+#     entry_field.delete(0, 'end')
+#
+#
+# root = Tk()
+#
+# Lb1 = Listbox(root, selectmode=MULTIPLE)
+# Lb1.insert(1, "Python")
+# Lb1.insert(2, "Perl")
+# Lb1.insert(3, "C")
+# Lb1.insert(4, "PHP")
+# Lb1.insert(5, "JSP")
+# Lb1.insert(6, "Ruby")
+#
+# Lb1.pack()
+#
+# quit_button = ttk.Button(text="Quit", command=root.destroy).pack()
+# valid_button = ttk.Button(text="Validate", command=selection).pack()
+#
+# start_button = ttk.Button(text="Start", command=start).pack()
+#
+# button = Button(root, text="Valider", command=recupVal).pack()
+#
+# label_field = Label(root, text="add a word").pack(side=LEFT)
+#
+# entry_field = Entry(root, bd=5)
+# entry_field.pack(side=RIGHT)
+#
+# root.mainloop()
 
 # --------------------------------------------------------------------------------------------------------------
 
